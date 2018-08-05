f8e87741c4d3b62348c8e6de78ac3ee67b93024b
bassy-IYui <1484isyi47@gmail.com>
2018-08-05 16:11:05 +0900

    テキストボックスに入力した値を掛け算する処理追加

---
+++ js/calculation.js
@@ -0,0 +1,7 @@
+function toMultiply(leftText, rightText) {
+    var total = 0;
+    var numLeft = parseFloat(leftText);
+    var numRight = parseFloat(rightText);
+    total = numLeft * numRight;
+    return total;
+}
--- js/clickRunButton.js
+++ js/clickRunButton.js
@@ -1,3 +1,6 @@
    function showResult() {
-       document.getElementById("result_label").innerText = "実行ボタンが押されました";
+       var leftText = document.getElementById("left_textbox").value;
+       var rightText = document.getElementById("right_textbox").value;
+       var total = toMultiply(leftText, rightText);
+       document.getElementById("result_label").innerText = total;
    }
