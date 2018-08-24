fde93eff081abba6a0cbfcd80bf71e650aced1e0
bassy-IYui <1484isyi47@gmail.com>
2018-08-24 22:55:47 +0900

    素数のみを昇順にソートする処理実装

--- js/sort.js
+++ js/sort.js
@@ -4,7 +4,13 @@ function sortText() {
     for (var i = 0; i < array.length; i++) {
         array[i] = parseFloat(array[i])
     }
-    array.sort(function(a, b) {
+    changeDescendingOrderText(array);
+    var primeArray = primeDecisionText(array);
+    changeAscendingOrderText(primeArray);
+}
+
+function changeDescendingOrderText(arrayInt) {
+    arrayInt.sort(function(a, b) {
         if (a > b) {
             return -1;
         }
@@ -13,6 +19,41 @@ function sortText() {
         }
         return 0;
     });
-    console.log(array);
-    document.getElementById("result_sort_text").innerText = array;
+    console.log(arrayInt);
+    document.getElementById("result_sort_text").innerText = arrayInt;
+}
+
+function changeAscendingOrderText(primeArray) {
+    primeArray.sort(function(a, b) {
+        if (a > b) {
+            return 1;
+        }
+        if (a < b) {
+            return -1;
+        }
+        return 0;
+    });
+    console.log(primeArray);
+    document.getElementById("result_sort_prime_text").innerText = primeArray;
+}
+
+function primeDecisionText(arrayInt) {
+    var primeArray = [];
+    for (var i = 0; i < arrayInt.length; i++) {
+        var num = arrayInt[i];
+        if (num === 2) {
+            primeArray.push(num);
+        } else {
+            for (var j = 2; j < num; j++) {
+                if (num % j === 0) {
+                    break
+                }
+                if (j + 1 === num) {
+                    primeArray.push(num);
+                }
+            }
+        }
+    }
+
+    return primeArray;
 }
